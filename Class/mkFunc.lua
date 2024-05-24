9fdcf676c6ccaf30cd9c283c529c0f67f314bb04
Word30210 <wordisnottoto@gmail.com>
2024-05-25 05:05:39 +0900

    Add instead of Class/mkMethod.lua

---
+++ Class/mkFunc.lua
@@ -0,0 +1,28 @@
+--#[ Variables ]#--
+local metatable = {
+    __type = "func";
+
+    __call = function(self, runner)
+        self.runner = runner
+        getmetatable(self).__call = nil
+        return self
+    end
+}
+
+--#[ Functions ]#--
+local function main(funcName: string)
+    local funcType = "unknownFunc"
+
+    local prefix, fnName = string.match(funcName, "([%.:]?)([%a_][%w_]+)")
+    if prefix == ":" then
+        funcType = "method"
+    elseif prefix == "." then
+        funcType = "default"
+    elseif string.find(fnName, "^__") then
+        funcType = "magic"
+    end
+
+    return setmetatable({ name = fnName or funcName, funcType = funcType, runner = print }, metatable)
+end
+
+return main
