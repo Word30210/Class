964d2baaaa3098d1d8e248b3e2bfded33627565e
Word30210 <wordisnottoto@gmail.com>
2024-05-26 10:24:06 +0900

    final test (maybe)

--- Testers/--mkFuncTest.lua
+++ Testers/--mkFuncTest.lua
@@ -1,24 +1,24 @@
 local mkFunc = require(script.Parent.Parent.mkFunc)
 
-local newFuncConfig1 = mkFunc ".test"
+-- local newFuncConfig1 = mkFunc ".test"
 
-print(newFuncConfig1)
-print(newFuncConfig1.funcType)
+-- print(newFuncConfig1)
+-- print(newFuncConfig1.funcType)
 
-local newFuncConfig1 = mkFunc ":test"
+-- local newFuncConfig1 = mkFunc ":test"
 
-print(newFuncConfig1)
-print(newFuncConfig1.funcType)
+-- print(newFuncConfig1)
+-- print(newFuncConfig1.funcType)
 
-local newFuncConfig1 = mkFunc "__test"
+-- local newFuncConfig1 = mkFunc "__test"
 
-print(newFuncConfig1)
-print(newFuncConfig1.funcType)
+-- print(newFuncConfig1)
+-- print(newFuncConfig1.funcType)
 
-local newFuncConfig1 = mkFunc "test"
+-- local newFuncConfig1 = mkFunc "test"
 
-print(newFuncConfig1)
-print(newFuncConfig1.funcType)
+-- print(newFuncConfig1)
+-- print(newFuncConfig1.funcType)
 
 local newFuncConfig2 = mkFunc ".test"
 
@@ -26,7 +26,7 @@ local runner1 = function()end
 local runner2 = function()end
 
 print(newFuncConfig2)
-print(getmetatable(newFuncConfig2).__type == "func")
+print(getmetatable(newFuncConfig2))
 
 newFuncConfig2(runner1)
 print(newFuncConfig2.runner == print)
