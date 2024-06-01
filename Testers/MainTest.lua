1124d3f5b5e59f836dd35103dbe5e8b47d40a851
Word30210 <wordisnottoto@gmail.com>
2024-06-02 02:49:09 +0900

    Update testers

--- Testers/MainTest.lua
+++ Testers/MainTest.lua
@@ -1,52 +1,61 @@
 local Class = require(script.Parent.Parent:WaitForChild("main"))
-local fn = Class.func
-local evt = Class.event
-local gp = Class.getProp
-local sp = Class.setProp
-local setProps = Class.setProps
+local def = Class.func
+local event = Class.event
+local get = Class.getProp
+local set = Class.setProp
+local super = Class.setProps
+local null = Class.null
 
-local newClass = Class "testClass" {
-	fn "__init" (function(self, inter, name)
-		setProps(self) {
+local myFrame = Class "Frame" {
+	def "__init" (function(self, internals, name)
+		super(self) {
+			color3 = Color3.new(1, 1, 1);
+			anchor = Vector2.new(0.5, 0.5);
+			position = UDim2.fromScale(0.5, 0.5);
+			size = UDim2.fromOffset(100, 100);
 			name = name;
-			__init = 1;
-		};
+			parent = null;
+		}
 
-		inter.test = "1q2w3e4r!"
+		local frame = Instance.new("Frame")
+		frame.BorderSizePixel = 0
+		frame.BackgroundColor3 = Color3.new(1, 1, 1)
+		frame.AnchorPoint = Vector2.new(0.5, 0.5)
+		frame.Position = UDim2.fromScale(0.5, 0.5)
+		frame.Size = UDim2.fromOffset(100, 100)
+		frame.Name = name
+
+		internals.frame = frame
+		internals.getInstanceKey = {
+			color3 = "BackgroundColor3";
+			anchor = "AnchorPoint";
+			position = "Position";
+			size = "Size";
+			name = "Name";
+			parent = "Parent";
+		}
 	end);
 
-	fn "__getter" (function(self, inter, key)
-		print(inter.test)
+	def "__setter" (function(self, internals, key, value)
+		set(self, key, value)
 
-		return gp(self, key)
+		internals.frame[internals.getInstanceKey[key]] = value
 	end);
 
-	fn "__call" (function(self, inter, ...)
-		print(...)
-		return "aaa"
+	def "__str" (function(self, internals)
+		return self.name
 	end);
 
-	fn "__concat" (function(self, inter, obj1, obj2)
-		return tostring(obj1) .. tostring(obj2)
-	end);
-
-	fn "__unm" (function(self, inter)
-		return -1
-	end);
+	event "testEvent"
 }
 
-local newObject1 = newClass.new("testName")
-local newObject2 = newClass.new("a")
+local Screen = Instance.new("ScreenGui")
+Screen.Parent = game.StarterGui
 
--- print(newObject1(1, 2, 3))
--- print("Is " .. newObject1 .. " not toto?")
--- print(-newObject1)
--- print(newObject1 == newObject2)
+local myNewFrame = myFrame.new("BackgroundFrame")
+myNewFrame.parent = Screen
 
-print(newObject1.name)
-
--- setProps(newObject1) {
--- 	name = 1;
--- }
+myNewFrame.testEvent:Connect(print)
+myNewFrame.testEvent:Fire "Hello, world!"
 
 return nil
