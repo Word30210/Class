6ebf1cf09518deaa5eaf428201acfba6e08ad7e0
Word30210 <wordisnottoto@gmail.com>
2024-06-02 02:48:41 +0900

    a little bit of change

--- Class/setProp.lua
+++ Class/setProp.lua
@@ -6,20 +6,13 @@ local assert = require(lib.customAssert)
 local function main(object, key, value)
     local metatable = getmetatable(object)
 
-    assert(not metatable.__objectCreated, "After the object is created, the value cannot be specified as \"setProp\"", 4)
-
     if metatable.__getableKeyDict[key] then
         if metatable.__setableKeyDict[key] then
-            if value then
-                metatable.__properties[key] = value
-            else
-                metatable.__getableKeyDict[key] = nil
-                metatable.__setableKeyDict[key] = nil
-            end
+            metatable.__properties[key] = value
         else
             assert(false, `{ tostring(object) }.{ key } is readonly`, 4)
         end
-    elseif value then
+    elseif value ~= nil then
         metatable.__getableKeyDict[key] = true
         metatable.__setableKeyDict[key] = true
         metatable.__properties[key] = value
