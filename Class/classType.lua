48821aaba02fb56a729905b36ecc718e7872ef6f
Word30210 <wordisnottoto@gmail.com>
2024-06-13 18:04:28 +0900

    Delete old file 'classType.lua'

--- Class/classType.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable: invalid-class-name
-
-return function(value: any, typeName)
-	if typeof(value) == "table" or typeof(value) == "userdata" then
-		return getmetatable(value).__type
-	else
-		return typeof(value, typeName)
-	end
-end
